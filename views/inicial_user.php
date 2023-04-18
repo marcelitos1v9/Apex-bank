1dabda960fff0bd85ab3880619a9c1d798e20028
Marcelo Augusto <123131698+marcelitos1v9@users.noreply.github.com>
2023-04-17 22:09:50 -0300

    Update inicial_user.php

--- views/inicial_user.php
+++ views/inicial_user.php
@@ -5,6 +5,10 @@
     <?php
     session_start();
     $user_id = $_SESSION['user_id'];
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: ./login.php');
+        exit;
+      }
     $sql = "SELECT * FROM clients WHERE id = $user_id";
     $result = mysqli_query($conn, $sql);
     while ($exibe = mysqli_fetch_array($result)) {
